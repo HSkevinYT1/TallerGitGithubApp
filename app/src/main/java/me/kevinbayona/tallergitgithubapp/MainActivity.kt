7580aa3ca3e8178ff39a065656635cc15241789d
HSkevinYT1 <ythskevin@gmail.com>
2026-09-16 15:45:59 -0500

    feat: agrega boton de contacto

--- app/src/main/java/me/kevinbayona/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/kevinbayona/tallergitgithubapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,7 +23,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -156,6 +160,26 @@ fun TerceraPantalla(){
                     fontSize = 16.sp,
                     color = Color.Gray
 
+                )
+
+
+
+                Spacer(modifier = Modifier.height(30.dp))
+
+                Text(
+                    text="Contactar conmigo",
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .background(
+                            Color.Blue,
+                            shape = RoundedCornerShape(12.dp)
+                        )
+                        .padding(
+                            all = 15.dp
+                        )
                 )
 
 
